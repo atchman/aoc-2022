119950fbc7c4090c8dd4299331a704fcb42bb644
atchman <atchman@hotmail.de>
2023-10-24 16:04:18 +0200

    day 2 solved part 2 solved

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Advent of Code 2022
 
+Doing Advent of Code to learn Python
+
 -[X] Day 1 solved  
--[ ] Day 2 solved  
+-[X] Day 2 solved  
 -[ ] Day 3 solved  
--- src/day02.py
+++ src/day02.py
@@ -1,33 +1,41 @@
-# day {{ num }}
+# day 02
 
 import os
 
 
-REL_PATH = os.path.relpath('../input/02-sample', start=os.path.pardir)
+REL_PATH = os.path.relpath('../input/02', start=os.path.pardir)
 PATH = os.path.abspath(REL_PATH)
 
 def main():
-    score = []
+    global score_part_one
+    global score_part_two
+    score_part_one = []
+    score_part_two = [] 
     
     with open(PATH, 'r') as FILE:
         for line in FILE:
-            player,myanswer = line.strip().split(' ')
+            player,me = line.strip().split(' ')
+            part_one(player, me)
+            part_two(player, me)
+     
+    print(score_part_one)
+    print(sum(score_part_one))
+    print(score_part_two)
+    print(sum(score_part_two))      
             
-            match myanswer:
-                case 'X':
-                    score.append(1 + round_outcome(player, 'A'))
-                case 'Y':
-                    score.append(2 + round_outcome(player, 'B'))
-                case 'Z':
-                    score.append( 3 + round_outcome(player, 'C'))
-                case _:
-                    print('ERROR')
-    print(score)
-    print(sum(score))
-        
-                 
-def round_outcome(player, myanswer):
+def part_one(player, myanswer):
+    match myanswer:
+        case 'X':
+            score_part_one.append(1 + round_outcome(player, 'A'))
+        case 'Y':
+            score_part_one.append(2 + round_outcome(player, 'B'))
+        case 'Z':
+            score_part_one.append(3 + round_outcome(player, 'C'))
+        case _:
+            print('ERROR')
     
+            
+def round_outcome(player, myanswer):
     if player == myanswer:
         return_value = 3
     else:
@@ -40,7 +48,40 @@ def round_outcome(player, myanswer):
         else:
             return_value = 0
     return return_value
+
+
+def part_two(player, myanswer):
+    match myanswer:
+        case 'X':
+            score_part_two.append(0)
+            match player:
+                case 'A':
+                    score_part_two.append(3)
+                case 'B':
+                    score_part_two.append(1)
+                case 'C':
+                    score_part_two.append(2)               
+        case 'Y':
+            score_part_two.append(3)
+            match player:
+                case 'A':
+                    score_part_two.append(1)
+                case 'B':
+                    score_part_two.append(2)
+                case 'C':
+                    score_part_two.append(3)    
+        case 'Z':
+            score_part_two.append(6)
+            match player:
+                case 'A':
+                    score_part_two.append(2)
+                case 'B':
+                    score_part_two.append(3)
+                case 'C':
+                    score_part_two.append(1)    
+        case _:
+            print('ERROR')
         
-        
+     
 if __name__ == '__main__':
     main()
